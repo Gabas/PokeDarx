fe5e00b891603fc1ce06bce22841a843447577f6
gabrielastdossantos <ast.gabriel2004@gmail.com>
2026-03-29 14:37:46 -0300

    first commit

---
+++ bin/pokedarx.dart
@@ -0,0 +1,5 @@
+import 'package:pokedarx/pokedarx.dart' as pokedarx;
+
+void main(List<String> arguments) {
+  print('Hello world: ${pokedarx.calculate()}!');
+}
---
+++ test/pokedarx_test.dart
@@ -0,0 +1,8 @@
+import 'package:pokedarx/pokedarx.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
