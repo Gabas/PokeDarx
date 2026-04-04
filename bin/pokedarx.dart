1fdc1a80517ef7acb0f61fd406a30cd1ebb2ef05
Luiz Augusto Manfron Matias <139803100+Burk087@users.noreply.github.com>
2026-04-04 15:38:53 -0300

    Quest 4/5

--- bin/pokedarx.dart
+++ bin/pokedarx.dart
@@ -1,8 +1,12 @@
 import 'package:pokedarx/pokemon.dart';
+import 'package:pokedarx/pokedarx.dart';
 
 void main() {
-  var chikorita = Pokemon(152, 'Chikorita', 'Planta', 5, 20, 20, false, 'Bayleef', 16,);
-  var cyndaquil = Pokemon(155, 'Cyndaquil', 'Fogo', 7, 24, 24, false, 'Quilava', 14,);
+  var chikorita = Pokemon(152, 'Chikorita', 'Planta', 5, 20, 20, false, 'Bayleef', 16);
+  var cyndaquil = Pokemon(155, 'Cyndaquil', 'Fogo', 7, 24, 24, false, 'Quilava', 14);
+  var totodile = Pokemon(158, 'Totodile', 'Agua', 10, 30, 30, true, 'Croconaw', 18);
+  var pikachu = Pokemon(25, 'Pikachu', 'Eletrico', 20, 35, 35, true, 'Raichu', 30);
+  var bulbasaur = Pokemon(1, 'Bulbasaur', 'Planta', 15, 25, 25, true, 'Ivysaur', 16); // Precisava ter 5 pokemons no minimo, adicionei mais 3 pra fica de acordo. Ass. LuizinhoBRgamer
 
   print('Estado inicial:');
   chikorita.exibirFicha();
@@ -21,4 +25,51 @@ void main() {
   print('Estado final:');
   chikorita.exibirFicha();
   cyndaquil.exibirFicha();
+
+
+
+  print('--- TESTE POKEDEX ---');
+
+  var pokedex = Pokedex();
+
+  pokedex.adicionarPokemon(chikorita);
+  pokedex.adicionarPokemon(cyndaquil);
+  pokedex.adicionarPokemon(totodile);
+  pokedex.adicionarPokemon(pikachu);
+  pokedex.adicionarPokemon(bulbasaur);
+
+  print('--- LISTA TODOS ---');
+  pokedex.listarTodos();
+
+  print('--- BUSCAR #25 ---');
+  var p = pokedex.buscarPorNumero(25);
+  if (p != null) {
+    p.exibirFicha();
+  }
+
+  print('--- REMOVER #158 ---');
+  print(pokedex.removerPokemonPorNumero(158));
+
+  print('--- CAPTURADOS ---');
+  for (var poke in pokedex.listarCapturados()) {
+    print(poke.nome);
+  }
+
+  print('--- TIPO PLANTA ---');
+  for (var poke in pokedex.listarPorTipo('planta')) {
+    print(poke.nome);
+  }
+
+  print('--- NIVEL > 10 ---');
+  for (var poke in pokedex.listarAcimaDoNivel(10)) {
+    print(poke.nome);
+  }
+
+  print('--- PODEM EVOLUIR ---');
+  for (var poke in pokedex.listarQuePodemEvoluir()) {
+    print(poke.nome);
+  }
 }
+
+
+
