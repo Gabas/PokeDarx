b88411918560d090eca8a2a31bcace95a041fca8
Luiz Augusto Manfron Matias <139803100+Burk087@users.noreply.github.com>
2026-04-04 15:26:11 -0300

    Update pokedarx.dart
    Questões 4/5

--- lib/pokedarx.dart
+++ lib/pokedarx.dart
@@ -1,3 +1,87 @@
-int calculate() {
-  return 6 * 7;
+import 'pokemon.dart';
+
+class Pokedex {
+  List<Pokemon> _pokemons = [];
+
+  // adiciona um pokemon na lista
+  bool adicionarPokemon(Pokemon p) {
+    for (var poke in _pokemons) {
+      if (poke.numero == p.numero) {
+        print('esse pokemon já ta na lista!');
+        return false;
+      }
+    }
+    _pokemons.add(p);
+    return true;
+  }
+
+
+  bool removerPokemonPorNumero(int numero) {
+    for (var poke in _pokemons) {
+      if (poke.numero == numero) {
+        _pokemons.remove(poke);
+        return true;
+      }
+    }
+    return false;
+  }
+
+  Pokemon? buscarPorNumero(int numero) {
+    for (var poke in _pokemons) {
+      if (poke.numero == numero) {
+        return poke;
+      }
+    }
+    return null;
+  }
+
+  void listarTodos() {
+    for (var poke in _pokemons) {
+      poke.exibirFicha();
+    }
+  }
+
+  List<Pokemon> listarCapturados() {
+    List<Pokemon> lista = [];
+    for (var p in _pokemons) {
+      if (p.capturado == true) {
+        lista.add(p);
+      }
+    }
+    return lista;
+  }
+
+  // filtro por tipo
+  List<Pokemon> listarPorTipo(String tipo) {
+    List<Pokemon> lista = [];
+    for (var p in _pokemons) {
+      if (p.tipo.toLowerCase() == tipo.toLowerCase()) {
+        lista.add(p);
+      }
+    }
+    return lista;
+  }
+
+  List<Pokemon> listarAcimaDoNivel(int nivelMinimo) {
+    List<Pokemon> lista = [];
+    for (var p in _pokemons) {
+      if (p.nivel > nivelMinimo) {
+        lista.add(p);
+      }
+    }
+    return lista;
+  }
+
+  // verifica se pode evoluir
+  List<Pokemon> listarQuePodemEvoluir() {
+    List<Pokemon> lista = [];
+    for (var p in _pokemons) {
+      if (p.proximaEvolucao != null) {
+        if (p.nivel >= p.nivelEvolucao) {
+          lista.add(p);
+        }
+      }
+    }
+    return lista;
+  }
 }
